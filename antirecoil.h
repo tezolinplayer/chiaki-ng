9c38497c195b35cc3b5a5f86016c11f5ebff0839
tezolinplayer <biduebob1995@gmail.com>
2026-01-17 09:08:34 -0300

    Create antirecoil.h

---
+++ antirecoil.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chiaki/ctrl.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Estrutura de configuração
+typedef struct {
+    bool enabled;
+    float vertical_strength;
+    int fire_button; // Ex: CHIAKI_CONTROLLER_BUTTON_R2
+} AntiRecoilConfig;
+
+// Funções principais
+void chiaki_antirecoil_init(void);
+void chiaki_antirecoil_process(ChiakiControllerState *state);
+
+#ifdef __cplusplus
+}
+#endif
