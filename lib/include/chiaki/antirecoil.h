d67ac37b6a10357764d4488a119f5aff9c39f780
tezolinplayer <biduebob1995@gmail.com>
2026-01-17 11:00:45 -0300

    Update antirecoil.h

--- lib/include/chiaki/antirecoil.h
+++ lib/include/chiaki/antirecoil.h
@@ -1,23 +1,25 @@
 #pragma once
 
-// --- CORREÇÃO DE OURO: Importar ctrl.h aqui ---
-#include <chiaki/ctrl.h> 
-// ----------------------------------------------
-#include <stdbool.h>
 #include <stdint.h>
+#include <stdbool.h>
+
+// --- CORREÇÃO: Importar as definições do controle aqui ---
+#include <chiaki/controller.h>
+#include <chiaki/ctrl.h>
+// ---------------------------------------------------------
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-// Estrutura de configuração
+// Configurações do Anti-Recoil
 typedef struct {
     bool enabled;
     float vertical_strength;
     int fire_button; // Ex: CHIAKI_CONTROLLER_BUTTON_R2
 } AntiRecoilConfig;
 
-// Funções principais
+// Funções
 void chiaki_antirecoil_init(void);
 void chiaki_antirecoil_process(ChiakiControllerState *state);
 
